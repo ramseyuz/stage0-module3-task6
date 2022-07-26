b1b085cb7df2618834ad8ee21e4ca190539beff0
Ramz <gulliyev95@gmail.com>
2022-07-26 09:57:56 +0500

    task6

--- src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
+++ src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
@@ -3,16 +3,16 @@ package lang.print.gaps.task6;
 public class OperatorPrecedence {
     public static void main(String[] args) {
         int first = 1;
-        int second = 2;
-        int eight = 3 + first * second;
+        int second = 3;
+        int eight = 5 + first * second;
         System.out.println(eight);//eight is expected to be 8
         boolean firstBool = false;
         boolean secondBool = false;
         boolean thirdBool = 1 == 1;
         boolean fourthBool = 4 <= 101;
-        int varSix = 17;
+        int varSix = 5;
 
-        System.out.println(firstBool && secondBool || thirdBool && fourthBool || (varSix = 5) < 0);
+        System.out.println(firstBool && secondBool && thirdBool && fourthBool && (varSix = 5) < 0);
         System.out.println(varSix);
     }
 }
